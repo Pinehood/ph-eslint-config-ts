d084441f589bed41c1a74a27128eadb07978f97a
fiki574 <fiki8796@hotmail.com>
2023-10-06 13:47:12 +0200

    Try something

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 module.exports = {
   globals: {},
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig2.json",
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin"],
   rules: {
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
+  },
+  root: true,
+  env: {
+    node: true,
+    jest: true,
   },
 };
